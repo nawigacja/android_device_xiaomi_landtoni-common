2be7c7195b56d23f8219a55f83e13fef7ae3f5c3
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-10 00:36:10 +0800

    MOD: landtoni-common: Welcome landtoni

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter land santoni,$(TARGET_DEVICE)),)
+ifneq ($(filter land santoni landtoni,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
